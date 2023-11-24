be03c5919a6a12064deaafbb9d930aff29a02b9c
RohitSignIn <rohitsinghsignin@gmail.com>
2023-11-24 16:37:36 +0530

    Added Some questions

--- one.py
+++ one.py
@@ -20,11 +20,68 @@
 
 # 1. SUM of all Entered Numbers
 # sum = 0
-# for i in [1,2,3,4,5]:  
-#     sum += sum + i
+# for i in [1,2,3,4,5]:
+      
+#     sum = sum + i
+
 # print(sum)
 
-# String
-for index, elem in enumerate("Hello"):
-    print(index, elem)
+# Reverse String
+# str = "Hello"
+# strLen = len(str)
 
+# for i in range(strLen-1, -1, -1):
+#     print(str[i], end="")    
+
+
+# Reverse String
+# str = "Hello"
+# a = ""
+# for i in str:
+#     a = i + a   
+
+# print(a)
+
+
+# 2. Count of Even No & Count of Odd Numbers
+# list=[1,2,3,4,5,6,7,8,9,10]
+# ec=0
+# oc=0
+# for i in list:
+#     if(i%2==0):
+#         ec+=1
+#     else:
+#         oc+=1
+# print(ec,oc)
+
+
+# 3. First Even No || First Odd No
+# list = [3,5,7,1,2,3,4,2,2]
+
+# for i in list:
+#     if(i % 2==0):
+#         print(i)
+#         break    
+
+# 4. Maximum Number
+# list = [2,4,5,5,7,6]
+# max = list[0]
+
+# for i in list:
+#     if(max < i):
+#         max = i
+
+# print(max)
+
+
+a = [22,3,11,444,12,13,88882,3,4]
+twoDigit = 0
+threeDigit = 0
+for i in a:
+    aLen = len(str(i))
+    if(aLen == 2):
+        twoDigit += i
+    elif(aLen == 3):
+        threeDigit += i
+
+print(twoDigit, threeDigit)
