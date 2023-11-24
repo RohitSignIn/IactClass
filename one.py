24350d5abde4c9f86ef752f2d5110eac7cb96afe
RohitSignIn <rohitsinghsignin@gmail.com>
2023-11-24 15:46:29 +0530

    Update on 24 Nov

---
+++ one.py
@@ -0,0 +1,30 @@
+# Even Odd 
+# a = input("Enter a Number: ")
+# a = int(a) 
+# if(a%2 == 0):
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# Max Number
+
+# print(list(range(1, 101, 1)))
+# a = [2,3,4,8,7]
+# max = a[0]
+# for i in a:
+#     if(max < i):
+#         max = i
+
+# print(max)
+
+# 1. SUM of all Entered Numbers
+# sum = 0
+# for i in [1,2,3,4,5]:  
+#     sum += sum + i
+# print(sum)
+
+# String
+for index, elem in enumerate("Hello"):
+    print(index, elem)
+
---
+++ test.py
@@ -0,0 +1,27 @@
+
+# n = "abcd"
+# n = str(n)
+# # "1234" 
+# length = len(n)
+
+# a = ""
+# for i in range(length-1, -1, -1):
+#     a = a + n[i]
+
+# print(int(a))
+
+# "2" + "3" = "23"
+
+# n = "2345"
+# a = ""
+# for i in n:
+#     a = i + a
+
+# print()
+
+
+sum = 0
+for i in [2,3,4,5]:
+    sum = sum + i
+
+print(sum)
